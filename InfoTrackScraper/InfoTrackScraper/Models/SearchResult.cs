4103b1fb12fb6a7cb7d559107edfc61b83e33345
Richard <richard.lucas@concave.co.uk>
2022-07-30 22:05:49 +0100

    Master - Added SearchResult class. Removed unused template views.

---
+++ InfoTrackScraper/InfoTrackScraper/Models/SearchResult.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InfoTrackScraper.Models {
+	public class SearchResult {
+		public DateTime SearchDateTime { get; set; } //TODO: come back and rename to something more appropriate
+		public int Location { get; set; }
+	}
+}
