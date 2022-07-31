f686b71e42e6bb585ae64c36212d4d81ebc0a234
Richard <richard.lucas@concave.co.uk>
2022-07-31 22:25:41 +0100

    Master - Added some temporary values for the SearchResults

--- InfoTrackScraper/InfoTrackScraper/Models/SearchViewModel.cs
+++ InfoTrackScraper/InfoTrackScraper/Models/SearchViewModel.cs
@@ -13,8 +13,10 @@ namespace InfoTrackScraper.Models {
 		public int? CurrentResult { get; set;} //TODO: This may need to be an array as the result may appear more than once
 
 		[Display(Name = "The past results have been:")]
-		public List<SearchResult> SearchResults { get; set; } = new List<SearchResult>(){
-			new SearchResult(){SearchDateTime = System.DateTime.Now, Location = 3}
+		public List<SearchResult> SearchResults { get; set; } = new List<SearchResult>(){ //TODO: These should be read in values
+			new SearchResult(){SearchDateTime = System.DateTime.Now, Location = 3},
+			new SearchResult(){SearchDateTime = System.DateTime.Now.AddDays(-346315), Location = 14},
+			new SearchResult(){SearchDateTime = System.DateTime.Now.AddHours(-3546315), Location = 15}
 		};
 
 		public string WebResponse {get; set;} = "";
